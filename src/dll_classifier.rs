c1632d0628674dfcb730ff852c1ac498c26956bd
AsmPrgmC3 <asmprgmc3@gmail.com>
2025-03-03 19:32:53 +0100

    Put DLL classification into its own module

---
+++ src/dll_classifier.rs
@@ -0,0 +1,79 @@
+use std::sync::LazyLock;
+
+use memchr::memmem;
+use regex::bytes::Regex;
+
+use crate::dll_parser::parse_dll;
+
+#[derive(Debug)]
+pub enum DllClassification {
+    Invalid,
+    NonDe,
+    Vanilla,
+    Rando(Option<RandoVersion>),
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub struct RandoVersion {
+    major: u64,
+    minor: u64,
+    patch: u64,
+}
+
+pub fn classify_file(file_data: &[u8]) -> DllClassification {
+    let Ok(heaps) = parse_dll(file_data) else {
+        return DllClassification::Invalid;
+    };
+
+    if !memmem::find(heaps.strings, b"SpiritGrenadeDamageDealer\0").is_some() {
+        return if memmem::find(heaps.strings, b"HoldingNightberryCondition\0").is_some() {
+            DllClassification::NonDe
+        } else {
+            DllClassification::Invalid
+        };
+    }
+
+    if !memmem::find(heaps.strings, b"Randomizer\0").is_some() {
+        return DllClassification::Vanilla;
+    }
+
+    return DllClassification::Rando(extract_rando_version(heaps.us));
+}
+
+fn extract_rando_version(us_heap: &[u8]) -> Option<RandoVersion> {
+    static VERSION_REGEX: LazyLock<Regex> = LazyLock::new(|| {
+        // Regex to find a version string literal embedded into the dll, e.g. "1.2.34"
+        // Format for a version string: <length prefix><version string><nul byte>
+        // Format for <version string>: UTF-16 encoded [\d+ '.' \d+ '.' \d+]
+        Regex::new(r#"(?-u).((?:\d\x00)+)\.\x00((?:\d\x00)+)\.\x00((?:\d\x00)+)\x00"#).unwrap()
+    });
+
+    VERSION_REGEX
+        .captures_iter(us_heap)
+        .filter_map(|m| {
+            let (full, [major, minor, patch]) = m.extract();
+
+            let length_prefix = full[0];
+            if length_prefix as usize != full.len() - 1 {
+                None
+            } else {
+                Some(RandoVersion {
+                    major: parse_trusted_utf16_number(major)?,
+                    minor: parse_trusted_utf16_number(minor)?,
+                    patch: parse_trusted_utf16_number(patch)?,
+                })
+            }
+        })
+        .max()
+}
+
+fn parse_trusted_utf16_number(bytes: &[u8]) -> Option<u64> {
+    let mut number = 0u64;
+
+    for i in (0..bytes.len()).step_by(2) {
+        let digit = (bytes[i] - b'0') as u64;
+        number = number.checked_mul(10)?.checked_add(digit)?;
+    }
+
+    Some(number)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use std::env::args;
-use std::sync::LazyLock;
 
-use memchr::memmem;
-use regex::bytes::Regex;
+use dll_classifier::classify_file;
 
-use crate::dll_parser::parse_dll;
-
+mod dll_classifier;
 mod dll_parser;
 
 fn main() {
@@ -40,68 +37,10 @@ fn main_impl() -> Result<Vec<String>, &'static str> {
         let file_name = file.file_name();
 
         let file_data = std::fs::read(file.path()).map_err(|_| "Couldn't read file")?;
-        let result = classify_file(&file_data);
 
-        results.push(format!("{}: {}", file_name.to_string_lossy(), result));
+        let result = classify_file(&file_data);
+        results.push(format!("{}: {:?}", file_name.to_string_lossy(), result));
     }
 
     Ok(results)
-}
-
-fn classify_file(file_data: &[u8]) -> String {
-    let heaps = match parse_dll(&file_data) {
-        Ok(heaps) => heaps,
-        Err(msg) => return format!("Invalid DLL: {msg}"),
-    };
-
-    if !memmem::find(heaps.strings, b"SpiritGrenadeDamageDealer\0").is_some() {
-        return "Unknown DLL".into();
-    }
-
-    if !memmem::find(heaps.strings, b"Randomizer\0").is_some() {
-        return "Vanilla Ori DE DLL".into();
-    }
-
-    static VERSION_REGEX: LazyLock<Regex> = LazyLock::new(|| {
-        // Regex to find a version string literal embedded into the dll, e.g. "1.2.34"
-        // Format for a version string: <length prefix><version string><nul byte>
-        // Format for <version string>: UTF-16 encoded [\d+ '.' \d+ '.' \d+]
-        Regex::new(r#"(?-u).((?:\d\x00)+)\.\x00((?:\d\x00)+)\.\x00((?:\d\x00)+)\x00"#).unwrap()
-    });
-
-    let version = VERSION_REGEX
-        .captures_iter(heaps.us)
-        .filter_map(|m| {
-            let (full, [major, minor, patch]) = m.extract();
-
-            let length_prefix = full[0];
-            if length_prefix as usize != full.len() - 1 {
-                None
-            } else {
-                Some((
-                    parse_utf16_number(major)?,
-                    parse_utf16_number(minor)?,
-                    parse_utf16_number(patch)?,
-                ))
-            }
-        })
-        .max();
-
-    match version {
-        Some((major, minor, patch)) => {
-            format!("Ori DE rando DLL (version {major}.{minor}.{patch})")
-        }
-        None => "Ori DE rando DLL (unknown version)".into(),
-    }
-}
-
-fn parse_utf16_number(bytes: &[u8]) -> Option<u64> {
-    let mut number = 0u64;
-
-    for i in (0..bytes.len()).step_by(2) {
-        let digit = (bytes[i] - b'0') as u64;
-        number = number.checked_mul(10)?.checked_add(digit)?;
-    }
-
-    Some(number)
 }
